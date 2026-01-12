1edf85ed06058b349266768bbdaa6b06f4e7fc59
Luca Gorosito <lucagoro04@gmail.com>
2026-01-12 09:53:50 -0300

    edit msg wsp

--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -1,7 +1,5 @@
 export function getWhatsappLink(phone, name) {
-  const message = `Hola ${name},
-Te aviso que tu cuota mensual se encuentra vencida.
-Cualquier cosa avisame y lo vemos.
+  const message = `Hola ${name}, te aviso que tu cuota mensual se encuentra vencida.
 ¡Gracias!`;
 
   return `https://wa.me/549${phone}?text=${encodeURIComponent(message)}`;
